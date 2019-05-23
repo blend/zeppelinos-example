8d4a32d326a337666e73c90d99f2e62cf3297020
ken <ken@blend.com>
2019-05-23 15:31:12 -0700

    3. Update the migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 const MetaCoin = artifacts.require("MetaCoin");
 
-module.exports = function(deployer) {
-  deployer.deploy(MetaCoin);
+module.exports = function(deployer, network, [rootAccount]) {
+  deployer.then(async () => {
+    if (['development', 'test'].indexOf(network) === -1) {
+      console.log(
+        'Skipping deployment of MetaCoin, which should be deployed with zos.'
+      );
+      return;
+    }
+    await deployer.deploy(MetaCoin);
+    const instance = await MetaCoin.deployed();
+    await instance.initialize(rootAccount);
+  });
 };
